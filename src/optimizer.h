f506c8b378e858fa7e67f411f13e8e4dbe6bc922
ArnauCinca <arnau.cinca@gmail.com>
2022-07-01 16:56:13 +0200

    fix automake

--- src/optimizer.h
+++ src/optimizer.h
@@ -1,4 +1,4 @@
-#include "../layer/layer.h"
+#include "layer.h"
 typedef struct optimizer_t{
   void (*optimize)(layer_t *in, layer_t *out, loss_function_t *loss, double lr, int batch, double ***outs, double ***fouts, double **real_outs, double ***deltas);
 } optimizer_t;
